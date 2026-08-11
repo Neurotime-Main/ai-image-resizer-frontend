c53f3934f05082b88a2f1c6edce82d31f3672dcd
neurotimeback2-cell <neurotimeback2@gmail.com>
2026-08-11 12:56:49 +0400

    check repo

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -73,6 +73,8 @@ export default function AppLayout() {
           </Dropdown>
         </header>
 
+        <h1>5555</h1>
+
         <div className="app-scroll">
           <Outlet />
         </div>
